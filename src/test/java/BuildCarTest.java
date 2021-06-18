a48fe19e454f4c694651192ba494f8618d080587
Thaina Watanabe <thaina.watanabe@hitechnologies.com.br>
2021-06-17 22:58:44 -0300

    changed test

--- src/test/java/BuildCarTest.java
+++ src/test/java/BuildCarTest.java
@@ -3,8 +3,8 @@ import Factory.CarroFactory;
 public class BuildCarTest {
     public static void main(String[] args) throws Exception {
         CarroFactory factory = new CarroFactory();
-        double motor = 3.0;
+        double motor = 1.4;
         long tamanhoPortaMala = 400;
-        factory.buildCar(motor, tamanhoPortaMala).toString();
+        factory.buildCar(motor, tamanhoPortaMala);
     }
 }
